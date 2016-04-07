a59d8dd8b39232e49b6824f537cb1b9a3f4d995b
Mateusz <mateuszwodan@gmail.com>
2016-04-07 22:30:11 +0200

    Dodanie skrypyow

---
+++ ZPI-projekt/Assets/scripts/back_to_game.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class back_to_game : MonoBehaviour
+{
+    public GameObject MainMenu; //Canvas
+    public GameObject Przyc1; //Przycisk powrotu do gry
+    public GameObject Przyc2; //Przycisk powrtu do menu
+    public GameObject Przyc3; //Przycisk pokazania celow misji
+    public GameObject opisMisji; //okno celu misji
+    public Text tekstmisji; //tekst celu misji
+    string trescMisji = " jakas tresc misji";
+    bool showMission = false;
+    // Use this for initialization
+    void Start()
+    {
+        opisMisji.SetActive(false);
+        HandleMenu();
+        tekstmisji.text = trescMisji;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            HandleMenu();          
+        }
+        
+    }
+
+    public void HandleMenu()
+    {
+       if(showMission)
+        {
+            opisMisji.SetActive(!opisMisji.activeSelf);
+            showMission = !showMission;
+        }
+            //toogle input
+            
+
+            //toogle menu
+            
+            Przyc1.SetActive(!Przyc1.activeSelf);
+            Przyc2.SetActive(!Przyc2.activeSelf);
+            Przyc3.SetActive(!Przyc3.activeSelf);
+
+
+    }
+    public void mission()
+    {
+        if(showMission == false)
+        {
+            opisMisji.SetActive(!opisMisji.activeSelf);
+            showMission = !showMission;
+            Przyc1.SetActive(false);
+            Przyc2.SetActive(false);
+            Przyc3.SetActive(false);
+        }
+    }
+   
+
+}
---
+++ ZPI-projekt/Assets/scripts/back_to_menu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class back_to_menu : MonoBehaviour {
+
+	// Use this for initialization
+	public void powrot()
+    {
+        Application.LoadLevel(0);
+    }
+
+}
