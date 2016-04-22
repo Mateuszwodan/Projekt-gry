90728415fbfca015bc532087e38a749b6a6381bd
Kamil Swiat <kamilswiat@gmail.com>
2016-04-22 11:48:17 +0200

    Skrypt broni, ale bez obrażeń

---
+++ ZPI-projekt/Assets/scripts/timeBomb.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class timeBomb : MonoBehaviour {
+
+    public float range_of_enemy = 15f;
+    public float range_of_boom = 30f;
+    public GameObject player;
+
+    void Update () {
+        if (Vector3.Distance(player.transform.position, transform.position) < range_of_enemy)
+        {
+            StartCoroutine(boom());
+        }
+
+    }
+
+    private IEnumerator boom()
+    {
+        yield return new WaitForSeconds(3f);
+        if (Vector3.Distance(player.transform.position, transform.position) < range_of_boom)
+        {
+            // Tu gracz traci życie
+        }
+        gameObject.SetActive(false);
+    }
+}
