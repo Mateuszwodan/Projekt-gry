1bafef27661bc4525b80ff0d6b53f63b2bf41c7f
Eron <pawel.sztelmach@wp.pl>
2016-04-26 14:47:57 +0200

    Dodanie 2 oraz 4 etapu budowy planszy oraz implementacje działania umocnien

---
+++ ZPI-projekt/Assets/scripts/DestroyWall.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyWall : MonoBehaviour {
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Burzacy")
+        {
+            other.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
