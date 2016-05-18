fc472d8c1d0569aa3971d1a0e4b724a1d4c6723c
Mateusz <mateuszwodan@gmail.com>
2016-05-18 18:57:26 +0200

    Napisy koncowe.

---
+++ ZPI-projekt/Assets/scripts/poprawione/Menu_nie_ruszac.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu_nie_ruszac : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void StartGame()
+    {
+        Application.LoadLevel(1);
+    }
+    public void Ending_text()
+    {
+        Application.LoadLevel(5);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
