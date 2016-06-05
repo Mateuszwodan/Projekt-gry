352443e098c754f4256b14e9d633b66ee3b5c3d4
Mateusz <mateuszwodan@gmail.com>
2016-06-05 18:35:57 +0200

    Dodanie intra

---
+++ ZPI-projekt/Assets/scripts/poprawione/intro.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class intro : MonoBehaviour {
+
+    double czas = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey)
+        {
+            Application.LoadLevel(0);
+        }
+        czas = czas + Time.deltaTime;
+        if(czas > 7)
+        {
+            Application.LoadLevel(0);
+        }
+    }
+}
